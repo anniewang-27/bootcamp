eb5257c1f5fcfb09a5a94cd889500155a9d2973a
Annie Wang <anniegw2004@gmail.com>
2022-06-23 13:47:47 -0400

    works for all numbers

--- 6.5slicing.py
+++ 6.5slicing.py
@@ -1,4 +1,15 @@
 text = "X-DSPAM-Confidence:    0.8475"
-pos = text.find("0")
+
+positions = [text.find("0"), text.find("1"), text.find("2"), text.find("3"), text.find("4"), text.find("5"), text.find("6"), text.find("7"), text.find("8"), text.find("9")]
+
+pos = None #smallest position of a number
+for num in positions:
+  if (num == -1):
+    continue
+  elif (pos == None) or (num < pos):
+    pos = num
+
+print(pos)
+
 num = text[pos:]
 print(str(num))
