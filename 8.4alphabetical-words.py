c817cb7dff067fd6a21c9648841b94686a20fb5e
Annie Wang <anniegw2004@gmail.com>
2022-07-07 18:08:07 -0400

    sorting and alphabetizing (words are lowercase)

---
+++ 8.4alphabetical-words.py
@@ -0,0 +1,13 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+  words = line.rstrip() #so that i am able to modify it
+  count = line.count(" ")
+  while count>0:
+    word = words[:words.index(" ")]
+    lst.append(word.lower())
+    words = words[words.index(" ")+1:]
+    count-=1
+
+print(sorted(lst))
